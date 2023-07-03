95cc503b29d5e9a8dfc81ee7cde64ebdf2a28f7a
Abdelhamid <abdelhamidofficial2005@gmail.com>
2023-07-03 18:12:43 +0300

    go

---
+++ server.js
@@ -0,0 +1,20 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router1 = jsonServer.router('database/articles.json');
+const router2 = jsonServer.router('database/projects.json');
+const middlewares = jsonServer.defaults();
+
+const PORT_ARTICLES = process.env.PORT_ARTICLES || 3001;
+const PORT_PROJECTS = process.env.PORT_PROJECTS || 3002;
+
+server.use('/articles', router1);
+server.use('/projects', router2);
+server.use(middlewares);
+
+server.listen(PORT_ARTICLES, () => {
+  console.log(`JSON Server (Articles) is running on port ${PORT_ARTICLES}`);
+});
+
+server.listen(PORT_PROJECTS, () => {
+  console.log(`JSON Server (Projects) is running on port ${PORT_PROJECTS}`);
+});
