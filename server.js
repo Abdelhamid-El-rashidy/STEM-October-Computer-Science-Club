d634d2fba6eef7c587a57828cdfcacfc962cbd57
Abdelhamid <abdelhamidofficial2005@gmail.com>
2023-07-03 21:07:07 +0300

    ser

--- server.js
+++ server.js
@@ -1,31 +1,23 @@
 const jsonServer = require('json-server');
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
-
 const server = jsonServer.create();
-const router1 = jsonServer.router('public/database/articles.json');
-const router2 = jsonServer.router('public/database/projects.json');
+const router1 = jsonServer.router('database/articles.json');
+const router2 = jsonServer.router('database/projects.json');
 const middlewares = jsonServer.defaults();
 
 const PORT_ARTICLES = process.env.PORT_ARTICLES || 3001;
 const PORT_PROJECTS = process.env.PORT_PROJECTS || 3002;
 
+// Serve static files
+server.use(jsonServer.static('public'));
+
 server.use('/articles', router1);
 server.use('/projects', router2);
 server.use(middlewares);
 
-// Create a basic HTTP server
-const httpServer = http.createServer((req, res) => {
-  // Serve static files
-  const filePath = path.join(__dirname, 'public', req.url);
-  const fileStream = fs.createReadStream(filePath);
-  fileStream.pipe(res);
+server.listen(PORT_ARTICLES, () => {
+  console.log(`JSON Server (Articles) is running on port ${PORT_ARTICLES}`);
 });
 
-httpServer.listen(PORT_ARTICLES, () => {
-  console.log(`JSON Server (Articles) is running on port ${PORT_ARTICLES}`);
-  httpServer.listen(PORT_PROJECTS, () => {
-    console.log(`JSON Server (Projects) is running on port ${PORT_PROJECTS}`);
-  });
+server.listen(PORT_PROJECTS, () => {
+  console.log(`JSON Server (Projects) is running on port ${PORT_PROJECTS}`);
 });
