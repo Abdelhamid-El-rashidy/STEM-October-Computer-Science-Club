ab4df4d040b3011bd7f127e76679ef5e5ef4c3e0
Abdelhamid <abdelhamidofficial2005@gmail.com>
2023-07-03 22:41:38 +0300

    fififi

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const jsonServer = require('json-server');
+const express = require('express');
+
 const server = jsonServer.create();
 const router = jsonServer.router('database/db.json');
 const middlewares = jsonServer.defaults();
 
 const PORT = process.env.PORT || 3001;
 
-server.use(jsonServer.static('public'));
+server.use(express.static('public'));
 server.use('/api', router);
 server.use(middlewares);
 
