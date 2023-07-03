014ad92a4f0d072c513d8a86311e256041c2d384
Abdelhamid <abdelhamidofficial2005@gmail.com>
2023-07-03 21:22:32 +0300

    db

--- server.js
+++ server.js
@@ -1,23 +1,14 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router1 = jsonServer.router('database/articles.json');
-const router2 = jsonServer.router('database/projects.json');
+const router = jsonServer.router('database/db.json');
 const middlewares = jsonServer.defaults();
 
-const PORT_ARTICLES = process.env.PORT_ARTICLES || 3001;
-const PORT_PROJECTS = process.env.PORT_PROJECTS || 3002;
+const PORT = process.env.PORT || 3001;
 
-// Serve static files
 server.use(jsonServer.static('public'));
-
-server.use('/articles', router1);
-server.use('/projects', router2);
+server.use('/api', router);
 server.use(middlewares);
 
-server.listen(PORT_ARTICLES, () => {
-  console.log(`JSON Server (Articles) is running on port ${PORT_ARTICLES}`);
-});
-
-server.listen(PORT_PROJECTS, () => {
-  console.log(`JSON Server (Projects) is running on port ${PORT_PROJECTS}`);
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
 });
